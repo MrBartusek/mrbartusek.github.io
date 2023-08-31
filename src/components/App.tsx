df516539f0d89fbdcb0c7f7a98bb9ae51800bf52
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-08-31 14:43:49 +0200

    Fix typo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,11 @@
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
+import './App.css';
+import ContactSection from './ContactSection';
+import Footer from './Footer';
 import HeroSection from './HeroSection';
 import Navbar from './Navbar/Navbar';
 import ProjectsSection from './ProjectsSection';
-import Footer from './Footer';
-import ReactGA from 'react-ga4';
-
-import './App.css';
-import { useEffect } from 'react';
-import ContactSection from './ContactSecion';
 
 function App() {
 
