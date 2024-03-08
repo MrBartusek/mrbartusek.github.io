9baf20776cfd814e045ecad28ac578c217535798
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-03-08 15:16:13 +0100

    Add stockedup as project and update skills

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -1,8 +1,7 @@
 import ProjectCard from './ProjectCard';
 import DokChatImage from '../assets/dokchat.png';
 import GifPickerReactImage from '../assets/gif-picker-react.png';
-import MeteoalarmCardImage from '../assets/meteoalarm-card.png';
-import ClashCalendarImage from '../assets/clash-calendar.png';
+import StockedUpImage from '../assets/stocked-up.png';
 import { useTranslation } from 'react-i18next';
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 import classNames from 'classnames';
@@ -16,6 +15,14 @@ function ProjectsList(props: ProjectsListProps) {
 
 	return (
 		<div {...props} className={classNames('flex flex-col gap-8 my-8', props.className)}>
+			<ProjectCard
+				title={t('projects.stocked-up.name')}
+				subTitle={t('projects.stocked-up.subtitle')}
+				demoLink='https://stockedup.dokurno.dev/'
+				codeLink='https://github.com/MrBartusek/stocked-up'
+				imageSrc={StockedUpImage}
+				description={t('projects.stocked-up.description')}
+			/>
 			<ProjectCard
 				title={t('projects.dokchat.name')}
 				subTitle={t('projects.dokchat.subtitle')}
@@ -31,22 +38,6 @@ function ProjectsList(props: ProjectsListProps) {
 				codeLink='https://github.com/MrBartusek/gif-picker-react'
 				imageSrc={GifPickerReactImage}
 				description={t('projects.gif-picker-react.description')}
-			/>
-			<ProjectCard
-				title={t('projects.meteoalarm-card.name')}
-				subTitle={t('projects.meteoalarm-card.subtitle')}
-				codeLink='https://github.com/MrBartusek/MeteoalarmCard'
-				homeAssistantForumLink='https://community.home-assistant.io/t/meteoalarmcard-2-0-weather-warnings-card-for-meteoalarm-meteo-france-environnement-canada-dwd-nina-and-more/438329'
-				imageSrc={MeteoalarmCardImage}
-				description={t('projects.meteoalarm-card.description')}
-			/>
-			<ProjectCard
-				title={t('projects.clash-calendar.name')}
-				subTitle={t('projects.clash-calendar.subtitle')}
-				demoLink='https://dokurno.dev/ClashCalendar/'
-				codeLink='https://github.com/MrBartusek/ClashCalendar'
-				imageSrc={ClashCalendarImage}
-				description={t('projects.clash-calendar.description')}
 			/>
 		</div>
 	);
