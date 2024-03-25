a652fcbdf77f1411edb2235373a6e420458c4ee0
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-03-25 12:25:07 +0100

    Make hero more responsive

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,8 +1,7 @@
-import { Trans, useTranslation } from 'react-i18next';
-import Section from './Section';
-import HeroSocialDrawer from './HeroSocialDrawer';
 import classNames from 'classnames';
-import { FaChevronDown } from 'react-icons/fa';
+import { Trans, useTranslation } from 'react-i18next';
+import HeroSocialDrawer from './HeroSocialDrawer';
+import Section from './Section';
 
 function HeroSection() {
 	const { t } = useTranslation();
@@ -17,11 +16,16 @@ function HeroSection() {
 				className={classNames(
 					'flex max-w-3xl items-center',
 					'h-[calc(75vh-88px)] lg:h-[calc(100vh-88px)]',
-					'text-center md:text-left',
+					'text-center lg:text-left',
 				)}
 			>
 				<div className="flex flex-1 flex-col justify-center">
-					<h1 className="mb-8 flex flex-col gap-2 text-4xl md:mb-12 md:gap-3 md:text-6xl">
+					<h1
+						className={classNames(
+							'mb-8 flex flex-col gap-2  md:mb-12 md:gap-3',
+							'text-4xl sm:text-5xl xl:text-6xl',
+						)}
+					>
 						<span className="font-semibold text-gray-200">
 							{t('hero.hello')}
 						</span>
@@ -30,7 +34,7 @@ function HeroSection() {
 						</span>
 					</h1>
 
-					<p className="mb-16 leading-6 md:text-2xl md:leading-8">
+					<p className="mb-16 leading-6 sm:text-xl md:text-2xl md:leading-8">
 						<Trans
 							i18nKey={'hero.about'}
 							t={t}
