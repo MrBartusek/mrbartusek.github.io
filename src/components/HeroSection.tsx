47a51416091df1f072f7b676921de5c9118184a0
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-03-25 11:59:37 +0100

    Update hero scaling

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -10,17 +10,18 @@ function HeroSection() {
 	return (
 		<Section
 			id="hero"
-			className="text-light bg-dark"
+			className="bg-dark text-light"
 			disablePadding
 		>
 			<div
 				className={classNames(
 					'flex max-w-3xl items-center',
 					'h-[calc(75vh-88px)] lg:h-[calc(100vh-88px)]',
+					'text-center md:text-left',
 				)}
 			>
 				<div className="flex flex-1 flex-col justify-center">
-					<h1 className="mb-12 flex flex-col gap-2 text-3xl sm:gap-3 sm:text-6xl">
+					<h1 className="mb-8 flex flex-col gap-2 text-4xl md:mb-12 md:gap-3 md:text-6xl">
 						<span className="font-semibold text-gray-200">
 							{t('hero.hello')}
 						</span>
@@ -29,7 +30,7 @@ function HeroSection() {
 						</span>
 					</h1>
 
-					<p className="text-md mb-16 leading-6 sm:text-xl sm:leading-8">
+					<p className="mb-16 leading-6 md:text-2xl md:leading-8">
 						<Trans
 							i18nKey={'hero.about'}
 							t={t}
