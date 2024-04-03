0b866bd30c5ae341c449b02c0498eea4e631f0a1
Bartosz Dokurno <bartusekcraft@gmail.com>
2024-04-03 21:18:06 +0200

    Improve language selector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,27 +1,35 @@
 import { useState } from 'react';
 import i18n, { SUPPORTED_LANGUAGES } from '../i18n';
+import classNames from 'classnames';
 
 function LanguageSelector() {
 	const [language, setLanguage] = useState(i18n.language);
 
-	function handleClick() {
-		i18n.changeLanguage(getNextLanguage()).catch(console.error);
+	function handleClick(code: string) {
+		i18n.changeLanguage(code).catch(console.error);
 		setLanguage(i18n.language);
 	}
 
-	function getNextLanguage(): string {
-		const currentLangIndex = SUPPORTED_LANGUAGES.indexOf(language);
-		const nextLangIndex = (currentLangIndex + 1) % SUPPORTED_LANGUAGES.length;
-		return SUPPORTED_LANGUAGES[nextLangIndex];
-	}
-
 	return (
-		<button
-			className="ml-5 border-l border-slate-600 pl-5 hover:underline"
-			onClick={handleClick}
+		<div
+			className={classNames(
+				'ml-4 border-l-2 border-gray-600 pl-3',
+				'flex divide-x divide-solid divide-gray-700',
+			)}
 		>
-			{getNextLanguage().toUpperCase()}
-		</button>
+			{SUPPORTED_LANGUAGES.map((code, key) => (
+				<button
+					className={classNames(
+						{ 'font-extrabold': language == code },
+						'px-2 hover:font-extrabold',
+					)}
+					onClick={() => handleClick(code)}
+					key={key}
+				>
+					{code.toUpperCase()}
+				</button>
+			))}
+		</div>
 	);
 }
 
